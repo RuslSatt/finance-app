1758cf0b75e4be26d66571c30d8070804f037352
Ruslan Sattarov <r8sattarov@mail.ru>
2024-09-24 20:31:06 +0500

    feat: Add Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-    <div></div>
+    <div>
+        <LoginPage />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginPage from './pages/login/ui/LoginPage.vue';
+</script>
 
 <style scoped></style>
---
+++ src/pages/login/ui/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="login">
+        <div class="login-container">
+            <div class="login-preview"></div>
+            <div class="login-form"></div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.login-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+}
+
+.login-preview {
+    flex: 0 0 50%;
+    background-color: #fff;
+}
+
+.login-form {
+    flex: 0 0 50%;
+    background-color: #686868;
+}
+</style>
