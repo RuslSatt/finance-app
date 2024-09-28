7ab7f27567b6590352ed8d321641449f7ba51a4f
Ruslan Sattarov <r8sattarov@mail.ru>
2024-09-28 19:50:32 +0500

    feat: Add home page

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,10 +1,23 @@
+import { HomePage } from '@/pages/home';
 import { LoginPage, RegistrationPage } from '@/pages/login';
+import type { VueElement } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [
-    { path: '/', component: LoginPage },
-    { path: '/login', component: LoginPage },
-    { path: '/registration', component: RegistrationPage }
+enum AppRoutes {
+    HOME = '/',
+    LOGIN = '/login',
+    REGISTRATION = '/registration'
+}
+
+interface RouterPath {
+    path: string;
+    component: VueElement;
+}
+
+const routes: RouterPath[] = [
+    { path: AppRoutes.HOME, component: HomePage },
+    { path: AppRoutes.LOGIN, component: LoginPage },
+    { path: AppRoutes.REGISTRATION, component: RegistrationPage }
 ];
 
 export const router = createRouter({
---
+++ src/pages/home/ui/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="home-page"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.home-page {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+</style>
