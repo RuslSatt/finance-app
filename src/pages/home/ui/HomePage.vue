635d5ccf90b6f30a8d00734af9cd3fd05194c3f5
Ruslan Sattarov <r8sattarov@mail.ru>
2024-10-03 21:35:51 +0500

    feat: Add sidebar

--- src/pages/home/ui/HomePage.vue
+++ src/pages/home/ui/HomePage.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="home-page">
+        <SideBar />
         <NavBar />
     </div>
 </template>
 
 <script setup lang="ts">
 import { NavBar } from '@/widgets/navbar';
+import { SideBar } from '@/widgets/sidebar';
 </script>
 
 <style scoped>
 .home-page {
     display: flex;
-    flex-direction: column;
     min-height: 100vh;
 }
 </style>
---
+++ src/widgets/sidebar/ui/SideBar.vue
@@ -0,0 +1,55 @@
+<template>
+    <aside class="sidebar">
+        <ul class="list">
+            <li class="item" v-for="item in data" :key="item.id">
+                <router-link class="item__link" :to="item.link" />
+                <span class="item__name">{{ item.name }}</span>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Item {
+    id: string;
+    name: string;
+    icon: string;
+    link: string;
+}
+
+const data = ref<Item[]>([
+    {
+        id: '1',
+        name: 'Dashboard',
+        icon: 'dashboard',
+        link: '/dashboard'
+    }
+]);
+</script>
+
+<style scoped>
+.sidebar {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 250px;
+    border-right: 1px solid var(--p-surface-200);
+}
+
+.list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.item {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
